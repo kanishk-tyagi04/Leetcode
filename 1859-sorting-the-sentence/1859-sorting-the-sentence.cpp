8fd2301b38f096899d1e10400eb8ebc03268ef4d
Kanishk Tyagi <111213277+kanishk-tyagi04@users.noreply.github.com>
2025-03-25 19:49:39 +0530

    Time: 3 ms (41.8%), Space: 8.6 MB (23.51%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string sortSentence(string s) {
-        string ans="";
-        string temp="";
-        vector<string>words(10);
+        string ans;
+        string temp;
+        vector<string>word(10);
         for(int i=0;i<s.size();i++){
-            temp+=s[i];
-            if(s[i]>='1' && s[i]<='9'){
+            temp += s[i];
+            if(s[i]>= '1' && s[i]<='9'){
                 temp.pop_back();
-                words[s[i]-'0']=temp;
+                word[s[i]-'0']=temp;
                 temp="";
                 i++;
             }
         }
         for(int i=0;i<10;i++){
-            if(words[i]!="")
-            ans+=words[i]+ ' ';
+            if(word[i]!=""){
+                ans+=word[i]+ ' ';
+            }
         }
         ans.pop_back();
         return ans;
