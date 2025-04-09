32e371afa767ba4f8e271677008efa072cd93227
Kanishk Tyagi <111213277+kanishk-tyagi04@users.noreply.github.com>
2025-04-09 10:53:57 +0530

    Time: 9 ms (54.08%), Space: 22.8 MB (96.71%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,27 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) { 
-          int k=0;
-          long long left=1, right= *max_element(piles.begin(),piles.end());          
-		    while(left<=right){
-               k = left + (right - left) / 2;
-                int count=0;
-             for(int i=0;i<piles.size();i++){
-                count+= (piles[i]+k-1)/k;
-                 
-             }
-             if(count>h){ 
-                
-                left=k+1;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+
+        while (left <= right) {
+            int k = left + (right - left) / 2;
+            long long count = 0;
+
+            for (int i = 0; i < piles.size(); i++) {
+                // Cast to long long before adding
+                count += ((long long)piles[i] + k - 1) / k;
             }
-            
-            else{
-                right=k-1;
+
+            if (count > h) {
+                left = k + 1;
+            } else {
+                right = k - 1;
             }
         }
-        
+
         return left;
     }
 };
