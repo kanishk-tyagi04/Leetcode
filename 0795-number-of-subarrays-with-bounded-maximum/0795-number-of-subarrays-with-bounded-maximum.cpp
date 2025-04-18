3fb8270cffd30683b2b2c9952ebff42a57d92a1f
Kanishk Tyagi <111213277+kanishk-tyagi04@users.noreply.github.com>
2025-04-18 12:24:13 +0530

    Time: 4 ms (28.57%), Space: 55.9 MB (82.86%) - LeetHub

--- 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.cpp
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
-        int count=0;
-        for(int start=0;start<nums.size();start++){
-            int maxVal=nums[start];
-            for(int end=start;end<nums.size();end++){
-                maxVal=max(maxVal, nums[end]);
-                if (maxVal >= left && maxVal <= right) {
-                    count++;
-                }
-                if (maxVal > right) break;
+    int countBoundedSubarrays(vector<int>& nums, int bound) {
+        int count = 0, curr = 0;
+        for (int num : nums) {
+            if (num <= bound) {
+                curr++;
+                count += curr;
+            } else {
+                curr = 0;
             }
         }
         return count;
+    }
+
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        return countBoundedSubarrays(nums, right) - countBoundedSubarrays(nums, left - 1);
     }
 };
