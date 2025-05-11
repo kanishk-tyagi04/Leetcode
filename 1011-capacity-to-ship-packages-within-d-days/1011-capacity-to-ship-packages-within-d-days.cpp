ebdef38b89066f7cdaefe934d30e5078fd9cc351
Kanishk Tyagi <111213277+kanishk-tyagi04@users.noreply.github.com>
2025-05-11 22:30:40 +0530

    Time: 11 ms (68.36%), Space: 55.3 MB (7.37%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+    bool isPossible(vector<int> weights, int capacity, int days){
+      
+        int weight = 0;
+        for(int i = 0; i < weights.size(); i++) {
+            if(weights[i] > capacity) {
+                return false;
+            } 
+            else if(weight + weights[i] > capacity) {
+                --days;
+                weight = weights[i];
+            } 
+            else {
+                weight += weights[i];
+            }
+        }
+        return days > 0;
+    }
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int totalSum = 0;
+        int maxWeight = 0;
+        for(int i = 0; i < weights.size(); i++) {
+            totalSum += weights[i];
+
+        }
+
+        int min =  totalSum / days;
+        int max = totalSum;
+
+        while(min < max) {
+            int capacity = (min + max) / 2;
+            if(isPossible(weights, capacity, days)) {
+                max = capacity;
+            } else {
+                min = capacity + 1;
+            }
+        }
+
+        return min                                                                                                                                                                                                                                                                                                                                                                                                                                                  ;
+    }
+};
+
