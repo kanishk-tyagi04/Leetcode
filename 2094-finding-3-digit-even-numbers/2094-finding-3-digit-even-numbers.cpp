6e21a24d90686b263a31e355e78b8030a07c5471
Kanishk Tyagi <111213277+kanishk-tyagi04@users.noreply.github.com>
2025-05-12 11:47:47 +0530

    Time: 391 ms (14.42%), Space: 13.8 MB (53.67%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    void backtrack(vector<int>& digits, vector<int>& temp, vector<bool>& used, set<int>& result) {
-        if (temp.size() == 3) {
-            int num = temp[0] * 100 + temp[1] * 10 + temp[2];
-            if (temp[0] != 0 && num % 2 == 0) {
-                result.insert(num);
-            }
-            return;
-        }
-
-        for (int i = 0; i < digits.size(); i++) {
-            if (used[i]) continue;
-            used[i] = true;
-            temp.push_back(digits[i]);
-            backtrack(digits, temp, used, result);
-            temp.pop_back();
-            used[i] = false;
-        }
-    }
-
     vector<int> findEvenNumbers(vector<int>& digits) {
-        set<int> result;
-        vector<int> temp;
-        vector<bool> used(digits.size(), false);
-        backtrack(digits, temp, used, result);
+        set<int>result;
+        int n=digits.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+             for(int k=0;k<n;k++){
+                if(i==j || j==k || k==i) continue;
+            int num = digits[i]*100+digits[j]*10+digits[k]*1;
+            if(digits[i]==0) continue;
+            if(num%2!=0) continue;
+            result.insert(num);
+        }
+        }
+        }
         return vector<int>(result.begin(), result.end());
     }
 };
