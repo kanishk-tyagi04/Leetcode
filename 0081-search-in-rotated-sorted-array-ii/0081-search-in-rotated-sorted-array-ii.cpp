1bfec1470c9090bf6c9e9dc5e16c24b7554ef04c
Kanishk Tyagi <111213277+kanishk-tyagi04@users.noreply.github.com>
2025-05-27 19:18:25 +0530

    Time: 0 ms (100%), Space: 18.1 MB (20.18%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        unordered_set<int>us;
-        for(int num:nums){
-            us.insert(num);
-        }
-        if(us.find(target)!=0){
-            return true;
+        int start=0;
+        int end=nums.size()-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+                start++;
+                end--;
+                continue;
+            }
+           if (nums[start] <= nums[mid]) {
+            if(target>=nums[start] && target<nums[mid]){
+                end=mid-1;
+           } else{
+                start=mid+1;
+            }
+            }
+            else{
+            if(target>nums[mid] && target<=nums[end]){
+                start=mid+1;
+           }else{
+                end=mid-1;
+            }
+            }
         }
         return false;
     }
