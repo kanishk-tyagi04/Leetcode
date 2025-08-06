b5f935129606c7580f21ff51e47e9707e7dcb61a
Kanishk Tyagi <111213277+kanishk-tyagi04@users.noreply.github.com>
2025-08-06 13:29:30 +0530

    Time: 0 ms (100%), Space: 35.7 MB (70.85%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int total_satisfied=0;
-        int extra_satisfied=0;
         for(int i=0;i<customers.size();i++){
             total_satisfied += (1-grumpy[i])*customers[i];
         }
+        int extra_satisfied=0;
         for(int i=0;i<minutes;i++){
             extra_satisfied += grumpy[i]*customers[i];
         }
         int max_extra_satisfied=extra_satisfied;
         for(int i=minutes;i<customers.size();i++){
-            extra_satisfied +=grumpy[i]*customers[i];
-            extra_satisfied -=grumpy[i-minutes]*customers[i-minutes];
-            max_extra_satisfied = max(max_extra_satisfied, extra_satisfied);
+            extra_satisfied += grumpy[i]*customers[i];
+            extra_satisfied -= grumpy[i-minutes]*customers[i-minutes];
+            max_extra_satisfied=max(max_extra_satisfied,extra_satisfied);
         }
         return total_satisfied+max_extra_satisfied;
+
     }
 };
